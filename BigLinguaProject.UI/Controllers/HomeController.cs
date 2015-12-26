556ead88206935ba44ddb63fe018860476c31a4b
maksimn <maksim1989sovunion@yandex.ru>
2015-12-26 04:49:14 +0300

    Start Front-End part of the user registration

---
+++ BigLinguaProject.UI/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Web.Mvc;
+using BigLinguaProject.UI.Models;
+
+namespace BigLinguaProject.UI.Controllers {
+    public class HomeController : Controller {
+        public ActionResult Index() {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(User user) {
+            return RedirectToAction("registered", user);
+        }
+        public ActionResult Registered(User user) {
+            return View(user);
+        }
+    }
+}
---
+++ BigLinguaProject.UI/Models/User.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace BigLinguaProject.UI.Models {
+    public class User {
+        public Int32 Id { get; set; }
+        public String Name { get; set; }
+        public String Password { get; set; }
+    }
+}
